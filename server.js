eafd467fa42d43505c6e8c4c4af48c2af9097063
ThamKub <s6452410012@sau.ac.th>
2025-03-28 12:05:45 +0700

    โย่ว

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const runRouter = require('./routes/run.route');
 
 require('dotenv').config();
 
+const http = require('http');
+
 const app = express(); //สร้าง Web Server
 
 const PORT = process.env.PORT || 5555; //เรียกใช้ค่า PORT จาก .env
